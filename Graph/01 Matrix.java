123f8054af8273fee04f8e73597b256db6610131
Madhav Bansal <122335374+Madhavbansal1@users.noreply.github.com>
2025-05-12 23:43:51 +0530

    Update 01 Matrix.java

--- Graph/01 Matrix.java
+++ Graph/01 Matrix.java
@@ -5,7 +5,10 @@ class Solution {
             return new int[0][0];
         
         Queue<int[]> q = new LinkedList<>();
-        for(int i=0; i<mat.length; i++){
+        for(int i=0; i<mat.length; i++){    
+
+
+            
             for(int j=0; j<mat[0].length; j++){
                 if(mat[i][j] == 0){
                     q.add(new int[]{i,j});
