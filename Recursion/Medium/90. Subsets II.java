124d4168aa24fe99546417eb331d16fb3ea914f3
Madhav Bansal <122335374+Madhavbansal1@users.noreply.github.com>
2025-05-22 14:11:11 +0530

    Create 90. Subsets II.java

---
+++ Recursion/Medium/90. Subsets II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums); 
+        List<List<Integer>> subsets = new ArrayList<>();
+        generateSubsets(nums, 0, new ArrayList<>(), subsets);
+        return subsets;
+    }
+
+    private void generateSubsets(int[] nums, int index, List<Integer> current, List<List<Integer>> subsets) {
+        subsets.add(new ArrayList<>(current)); 
+
+        for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue; 
+            }
+            current.add(nums[i]);
+            generateSubsets(nums, i + 1, current, subsets); 
+            current.remove(current.size() - 1); 
+        }
+    }
+}
